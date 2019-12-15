62da0691c844e50cfacb1c7bb1b90949f8a27ccf
Tandi Samples <tandisamples@gmail.com>
2019-12-15 15:09:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,12 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 get '/newteam' do
-  @name = params[:name]
-  erb :newteam
-end
-post '/team' do
-  @name = "Bballers"
-  erb :team
-end
+    erb :newteam
+  end
+
+  post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+    erb :team
+  end
 
 end
